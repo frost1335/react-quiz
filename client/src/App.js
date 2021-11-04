ad24b39037885a3e46675f352e128e273f7a2eff
raximvodil <raximvodilrozbek@gmail.com>
2021-11-04 17:55:25 +0500

    6-block 4-video

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,15 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import Layout from "./hoc/Layout/Layout";
+import Quiz from "./containers/Quiz/Quiz";
+
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        <Quiz />
+      </Layout>
+    )
+  }
 }
 
 export default App;
