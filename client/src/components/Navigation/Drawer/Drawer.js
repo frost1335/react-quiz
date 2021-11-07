fb3f3f6236ae7c6343cbacfd851cedbe454a023f
raximvodil <raximvodilrozbek@gmail.com>
2021-11-07 10:56:33 +0500

    6-block 15-video finished

---
+++ client/src/components/Navigation/Drawer/Drawer.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import classes from "./Drawer.module.css";
+
+const links = [1, 2, 3];
+
+class Drawer extends Component {
+  renderLinks() {
+    return links.map((link, index) => {
+      return (
+        <li key={index}>
+          <a>Link {link}</a>
+        </li>
+      );
+    });
+  }
+
+  render() {
+    const cls = [classes.Drawer];
+
+    if (!this.props.isOpen) {
+      cls.push(classes.close);
+    }
+
+    return (
+      <nav className={cls.join(" ")}>
+        <ul>{this.renderLinks()}</ul>
+      </nav>
+    );
+  }
+}
+
+export default Drawer;
--- client/src/hoc/Layout/Layout.js
+++ client/src/hoc/Layout/Layout.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import classes from "./Layout.module.css";
 import MenuToggle from "../../components/Navigation/MenuToggle/MenuToggle";
+import Drawer from "../../components/Navigation/Drawer/Drawer";
 
 class Layout extends Component {
   state = {
@@ -16,6 +17,8 @@ class Layout extends Component {
   render() {
     return (
       <div className={classes.Layout}>
+        <Drawer isOpen={this.state.menu} />
+
         <MenuToggle
           onToggle={this.toggleMenuHandler}
           isOpen={this.state.menu}
